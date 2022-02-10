72c09b5c5787a978b4971769339ea1bb7d6f61a9
DigitalBe <digitalB@protonmail.com>
2022-02-10 15:21:07 +0100

    files copied

---
+++ script.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('scroll', fadeIn);
+
+function fadeIn() {
+    const text = document.querySelectorAll(".container");
+    const textArr = Array.from(text);
+    
+    for (let t in textArr) {
+        let windowHeight = window.innerHeight;
+        let revealT = textArr[t].getBoundingClientRect().top;
+
+        if (revealT < windowHeight) {
+            text[t].classList.add('active');
+        } 
+        else {
+            text[t].classList.remove('active');
+        }
+
+    }
+}
+
+
+const menu = document.getElementById('menu');
+const closeIcon = document.getElementById("toggle");
+
+    menu.addEventListener('click', handleMenuClick);
+
+    function handleMenuClick(event) {
+      if (event.target instanceof HTMLAnchorElement) {
+        closeIcon.checked = false;
+      }
+    }
